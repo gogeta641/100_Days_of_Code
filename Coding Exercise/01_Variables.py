d7ff568b6965f0108a948287e0e21315b99bf2c6
gogeta641 <karthik.gogeta@gmail.com>
2023-10-01 17:42:09 +0530

    Finished first day project

---
+++ Coding Exercise/01_Variables.py
@@ -0,0 +1,20 @@
+# Exercise 1
+print('Exercise 1')
+name = input('What is your name?')
+length = len(name)
+print(length)
+
+# Exercise 2
+print('Exercise 2')
+
+# There are two variables, a and b
+a = input()
+b = input()
+
+# Create a third variable to help switch the values
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
---
+++ Projects/01_Band_Name_Generator.py
@@ -0,0 +1,16 @@
+# Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+
+# Ask the user for the city that they grew up in.
+print("What's the name of the city you grew up in?")
+city = input()
+
+# Ask the user for the name of a pet.
+print("What's your pet's name?")
+pet = input()
+
+# Combine the name of thier city and pet and show them theird band name.
+print('Your band name could be ' + city + ' ' + pet)
+
+# Make sure the input cursor shows on a new line.
+print()
